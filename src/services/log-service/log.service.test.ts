55915f1bd066b7a2afc596e3aca7a46186c3c38a
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2019-04-15 21:27:11 -0400

    Add lodash-es for utility and luxon for dates.

--- src/services/log-service/log.service.test.ts
+++ src/services/log-service/log.service.test.ts
@@ -1,5 +1,10 @@
 import { LogService } from './log.service';
 
+// import { get } from 'lodash-es'
+// get({a:1}, 'a')
+
+// import { DateTime } from 'luxon';
+
 describe('logService', function() {
   let service: LogService;
 
