103194734b94b99cd415d8bb1fe9ac24ec747a07
patrickneugebauer <patrickneugebauer@users.noreply.github.com>
2019-04-15 23:36:56 -0400

    Have app create its own root, don't rely on index.html.

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,16 @@ export const appModule = angular
     servicesModule.name,
     componentsModule.name,
     featuresModule.name
-  ])
+  ]);
+
+// create a root for the app
+// create a new blank div in the body, with a ui-view inside
+const appRootElement = document.createElement('div');
+const uiViewRootElement = document.createElement('ui-view');
+appRootElement.appendChild(uiViewRootElement);
+document.body.appendChild(appRootElement);
 
 angular.bootstrap(
-  document.getElementById('app'),
+  appRootElement,
   [appModule.name]
 )
